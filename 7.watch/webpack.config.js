9475b3a08c5dcc76bd07e005f3f7d37e2b95ad85
ShenQiuhui <pandashen821@163.com>
2021-06-24 16:24:58 +0800

    feat: watch

---
+++ 7.watch/webpack.config.js
@@ -0,0 +1,46 @@
+'use strict'
+
+const path = require('path');
+
+module.exports = {
+  entry: {
+    index: './src/index.js',
+    search: './src/search.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        use: 'babel-loader'
+      },
+      {
+        test: /.css$/,
+        // 链式调用和执行顺序从右到左
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /.less$/,
+        use: ['style-loader', 'css-loader', 'less-loader']
+      },
+      {
+        test: /.(png|jpg|gif|jpeg)$/,
+        // use: 'file-loader'
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 10240
+          }
+        }]
+      },
+      {
+        test: /.(woff|woff2|eot|ttf|otf|ttc)$/,
+        use: 'file-loader'
+      }
+    ]
+  }
+};
