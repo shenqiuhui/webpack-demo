8b9df32005d610ade2f189b839ba2515fa8967bd
ShenQiuhui <pandashen821@163.com>
2021-07-17 18:08:19 +0800

    feat: optmize speed

---
+++ 28.optmize-speed/dist/256.036fc5d9.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_28_optmize_speed=self.webpackChunk_28_optmize_speed||[]).push([[256],{256:function(e,t,n){"use strict";n.r(t);var u=n(804),p=n.n(u);t.default=function(){return p().createElement("div",null,"动态 import")}}}]);
---
+++ 28.optmize-speed/dist/commons.ce80ec55.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_28_optmize_speed=self.webpackChunk_28_optmize_speed||[]).push([[351],{669:function(e,n,t){"use strict";function u(){return"Hello webpack"}t.d(n,{T:function(){return u}})}}]);
